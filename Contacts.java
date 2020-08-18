89ac8c7da7feb0068cb4bfc2f8491f68934b6233
Erdaulet <47812867+Can1s@users.noreply.github.com>
2020-08-18 11:44:39 +0600

    Stage 1/4
    Completed the first stage.

---
+++ Contacts.java
@@ -0,0 +1,12 @@
+package contacts;
+
+class Contacts {
+    String name;
+    String surname;
+    String number;
+    Contacts (String name, String surname, String number) {
+        this.name = name;
+        this.surname = surname;
+        this.number = number;
+    }
+}
