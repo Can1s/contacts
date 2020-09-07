f612c2c8c64cc365a14ee17121db7d3057125c52
Erdaulet <47812867+Can1s@users.noreply.github.com>
2020-09-07 14:06:20 +0600

    Stage 3/4
    Completed the third stage

---
+++ Person.java
@@ -0,0 +1,41 @@
+package contacts;
+
+public class Person extends Element {
+    private String name;
+    private String surname;
+    private String birthDay;
+    private String gender;
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+}
