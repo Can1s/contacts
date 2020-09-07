f980ea925869f017c9e3ab6a3b113968d7773760
Erdaulet <47812867+Can1s@users.noreply.github.com>
2020-09-07 21:26:54 +0600

    Stage 4/4
    Completed the fourth stage and the project

--- Element.java
+++ Element.java
@@ -1,10 +1,42 @@
 package contacts;
 
-public class Element {
+import java.util.ArrayList;
+
+public abstract class Element {
     private String phone;
-    private boolean isPerson;
-    private String creationDate;
+    private final String creationDate;
     private String lastEditDate;
+
+    public Element(String phone, String creationDate, String lastEditDate) {
+        this.phone = phone;
+        this.creationDate = creationDate;
+        this.lastEditDate = lastEditDate;
+    }
+
+    public void getAllFields() {
+    }
+
+    public ArrayList<String> getAllItemField() {
+        return null;
+    }
+
+    public void changeField(String field, String volume) {
+
+    }
+
+    public String getField(String field) {
+        switch (field) {
+            case "phone":
+                return this.getPhone();
+            case "creationDate":
+                return this.getCreationDate();
+            case "lastEditDate":
+                return this.getLastEditDate();
+            default:
+                return null;
+        }
+    }
+
 
     public String getPhone() {
         return phone;
@@ -14,20 +46,8 @@ public class Element {
         this.phone = phone;
     }
 
-    public boolean isPerson() {
-        return isPerson;
-    }
-
-    public void setPerson(boolean person) {
-        isPerson = person;
-    }
-
     public String getCreationDate() {
         return creationDate;
-    }
-
-    public void setCreationDate(String creationDate) {
-        this.creationDate = creationDate;
     }
 
     public String getLastEditDate() {
