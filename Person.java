aa6343a9193b5fd1663baec07bd710dd510a273a
Erdaulet <47812867+Can1s@users.noreply.github.com>
2020-09-07 21:29:03 +0600

    Stage 4/4
    Completed the fourth stage and the project

--- Person.java
+++ Person.java
@@ -1,10 +1,91 @@
 package contacts;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Person extends Element {
     private String name;
     private String surname;
     private String birthDay;
     private String gender;
+
+    public Person(String name, String surname, String birthDay, String gender,
+                  String phone, String creationDate, String lastEditDate) {
+        super(phone, creationDate, lastEditDate);
+        this.name = name;
+        this.surname = surname;
+        this.birthDay = birthDay;
+        this.gender = gender;
+    }
+
+    @Override
+    public void getAllFields() {
+        System.out.println("Name: " + this.getName());
+        System.out.println("Surname: " + this.getSurname());
+        System.out.println("Birth date: " + this.getBirthDay());
+        System.out.println("Gender: " + this.getGender());
+        System.out.println("Number: " + this.getPhone());
+        System.out.println("Time created: " + this.getCreationDate().substring(0, 16));
+        System.out.println("Time last edit: " + this.getLastEditDate().substring(0, 16));
+    }
+
+    @Override
+    public ArrayList<String> getAllItemField() {
+        ArrayList<String> items = new ArrayList<>();
+        items.add(this.getName());
+        items.add(this.getSurname());
+        items.add(this.getBirthDay());
+        items.add(this.getGender());
+        items.add(this.getPhone());
+        items.add(this.getCreationDate().substring(0, 16));
+        items.add(this.getLastEditDate().substring(0, 16));
+        return items;
+    }
+
+    @Override
+    public void changeField(String field, String volume) {
+        switch (field) {
+            case ("name"):
+                this.setName(volume);
+                break;
+            case ("surname"):
+                this.setSurname(volume);
+                break;
+            case ("birth"):
+                this.setBirthDay(volume);
+                break;
+            case ("gender"):
+                this.setGender(volume);
+                break;
+            case ("number"):
+                this.setPhone(volume);
+                break;
+        }
+        this.setLastEditDate(String.valueOf(LocalDateTime.now()));
+    }
+
+    @Override
+    public String getField(String field) {
+        switch (field) {
+            case ("name"):
+                return this.getName();
+            case ("surname"):
+                return this.getSurname();
+            case ("birth"):
+                return this.getBirthDay();
+            case ("gender"):
+                return this.getGender();
+            case "phone":
+                return this.getPhone();
+            case "creationDate":
+                return this.getCreationDate();
+            case "lastEditDate":
+                return this.getLastEditDate();
+
+            default:
+                return null;
+        }
+    }
 
     public String getBirthDay() {
         return birthDay;
